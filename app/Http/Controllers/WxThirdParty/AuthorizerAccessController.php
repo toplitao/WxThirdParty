966213ca334f6a611c9f3806f23bf2a14e3a75a9
litao <litao@yilu-tech.com>
2017-12-29 18:12:32 +0800

    修改

--- app/Http/Controllers/WxThirdParty/AuthorizerAccessController.php
+++ app/Http/Controllers/WxThirdParty/AuthorizerAccessController.php
@@ -11,7 +11,8 @@ class AuthorizerAccessController extends Controller
     public function getComponentLoginPage(){
         $wx=new WxThirdPartyService();
         $pre_auth_code=$wx->getPreAuthCode()['pre_auth_code'];
-        $url="https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid=$wx->appId&pre_auth_code=$pre_auth_code&redirect_uri=http://auth.event.wx.yilu.co&auth_type=3";
+        $authdomain=env('authdomain');
+        $url="https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid=$wx->appId&pre_auth_code=$pre_auth_code&redirect_uri=$authdomain&auth_type=3";
         return view('test', ['url' => $url]);
     }
 }
